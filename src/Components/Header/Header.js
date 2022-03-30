4db975c019452f1ff116f30e846f55cb42b83291
kakon4366 <kakonbarman4366@gmail.com>
2022-03-30 11:29:45 +0600

    Header component added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,8 +2,28 @@ import React from "react";
 
 const Header = () => {
 	return (
-		<div>
-			<h1 className="text-6xl font-bold underline">Hello world!</h1>
+		<div className="header bg-gray-700 text-white py-4">
+			<div className="container mx-auto">
+				<nav className="flex justify-between">
+					<h4 className="text-2xl">Logo</h4>
+					<ul className="flex">
+						<li className="mr-12">
+							<a className="text-lg" href="/">
+								Home
+							</a>
+						</li>
+						<li className="mr-12">
+							<a href="/about">About</a>
+						</li>
+						<li className="mr-12">
+							<a href="/restaurant">Restaurant</a>
+						</li>
+						<li className="mr-12">
+							<a href="/cart">Cart</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</div>
 	);
 };
